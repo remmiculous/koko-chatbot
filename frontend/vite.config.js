98232b60ec16e2d897319c14906092c07f1b4037
Remmi <remmiculous@gmail.com>
2026-01-12 00:57:33 +0530

    front end hosting fixes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,15 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-	plugins: [react()],
-	define: {
-		"process.env.NODE_ENV": JSON.stringify("production"),
-	},
-	build: {
-		lib: {
-			entry: "src/index.jsx",
-			name: "VetChatbot",
-			fileName: "chatbot",
-		},
-	},
+  plugins: [react()],
+  define: {
+    "process.env.NODE_ENV": JSON.stringify("production"),
+  },
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    lib: {
+      entry: "src/index.jsx",
+      name: "VetChatbot",
+      formats: ["iife"],
+      fileName: () => "chatbot.js",
+    },
+  },
 });
